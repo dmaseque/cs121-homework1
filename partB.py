7a1dfbcfc3a6db662dd59da150850c3266cc69ef
dmaseque <dmaseque@uci.edu>
2025-01-21 20:20:23 -0800

    Set up partB.py file

---
+++ partB.py
@@ -0,0 +1,12 @@
+import PartA as A 
+import sys
+
+if __name__ == '__main__':
+    text_file_1 = sys.argv[1]
+    text_file_2 = sys.argv[2]
+
+    file1_tokens = A.tokenize(text_file_1)
+    file2_tokens = A.tokenize(text_file_2)
+
+    
+
