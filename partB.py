eef238f3ea39af6b3ff2d2b354d97c5c648bef80
dmaseque <dmaseque@uci.edu>
2025-01-26 10:21:20 -0800

    Change while loop to use indexes instead of iterators. Make token list a set before sorting and algorithm.

--- partB.py
+++ partB.py
@@ -5,32 +5,26 @@ if __name__ == '__main__':
     text_file_1 = sys.argv[1]
     text_file_2 = sys.argv[2]
 
-    file1_tokens = A.tokenize(text_file_1)
-    file2_tokens = A.tokenize(text_file_2)
+    file1_tokens = list(set(A.tokenize(text_file_1)))
+    file2_tokens = list(set(A.tokenize(text_file_2)))
 
     file1_tokens.sort()
     file2_tokens.sort()
 
     #Algorithm 3: Sorted Lists Approach from Discussion Week 2 slides
     R = []
-    file1_iter = iter(file1_tokens)
-    file2_iter = iter(file2_tokens)
-    try:
-        file1_val = next(file1_iter)
-        file2_val = next(file2_iter)
-        while True:
-            if file1_val < file2_val:
-                file1_val = next(file1_iter)
-            elif file2_val < file1_val:
-                file2_val = next(file2_iter)
-            else:
-                R.append(file1_val)
-                file1_val = next(file1_iter)
-                file2_val = next(file2_iter)
-    except StopIteration:
-        pass
+    file1_index = 0
+    file2_index = 0
+    while file1_index < len(file1_tokens) and file2_index < len(file2_tokens):
+        if file1_tokens[file1_index] < file2_tokens[file2_index]:
+            file1_index += 1
+        elif file2_tokens[file2_index] < file1_tokens[file1_index]:
+            file2_index += 1
+        else:
+            R.append(file1_tokens[file1_index])
+            file1_index += 1
+            file2_index += 1
     
-    
-    print(len(set(R)))
+    print(len(R))
 
 
